9d5cfa6f153dde634652113a879831ae082b8a3d
svbiz4u <136245961+svbiz4u@users.noreply.github.com>
2024-08-29 20:55:01 +0200

    com-29082024-2054

--- js/task-3.js
+++ js/task-3.js
@@ -6,23 +6,22 @@ class StringBuilder  {
 
 getValue() {
 return this.#value;
-} // — повертає поточне значення приватної властивості value.
+} 
 
 padEnd(str) {
-    this.#value += str;   // Метод для додавання рядка в кінець
-    // this.#value.push(str);
-} //  — отримує параметр str (рядок) і додає його в кінець значення приватної властивості value об'єкта, який викликає цей метод.
+    this.#value += str;  
+  
+} 
 
 padStart(str) {
-    this.#value = str + this.#value;   // Метод для додавання рядка на початок
-// this.#value.unshift(str)
-} // — отримує параметр str (рядок) і додає його на початок значення приватної властивості value об'єкта, який викликає цей метод.
+    this.#value = str + this.#value;   
+
+} 
 
 padBoth(str) {
-    this.#value = str + this.#value + str;   // Метод для додавання рядка на початок і в кінець
-    // this.#value.push(str)
-    // this.#value.unshift(str)
-} //  — отримує параметр str (рядок) і додає його на початок і в кінець значення приватної властивості value об'єкта, який викликає цей метод.
+    this.#value = str + this.#value + str; 
+    
+} 
 }
 
 const builder = new StringBuilder(".");
